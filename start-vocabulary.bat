af6b5bd08df67ec93c47ca529d6990d3a05ab047
p1eXu5 <v1adp1exu5@gmail.com>
2025-07-01 09:36:04 +0300

    infr: edit bat, remove image prune cmd, explicit service name

--- start-vocabulary.bat
+++ start-vocabulary.bat
@@ -1,2 +1,9 @@
-docker-compose -f docker-compose.yml up --force-recreate --build -d
-docker image prune -f
+setlocal
+
+docker-compose -f docker-compose.yml down -v    vocabulary
+docker-compose -f docker-compose.yml up --force-recreate --build -d    vocabulary
+
+endlocal
+
+PAUSE
+exit /b 0
