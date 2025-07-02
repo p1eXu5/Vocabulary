e5c22dc24017510cf6c9e1410550ef4f85a49dd8
p1eXu5 <v1adp1exu5@gmail.com>
2025-07-02 21:47:21 +0300

    wip: add GridSplitter

---
+++ src/Vocabulary.BlazorServer/Shared/Components/GridSplitter.razor.js
@@ -0,0 +1,34 @@
+﻿var mudDrawerResizeFactory = function (dotNetRef) {
+    var _dotNetRef = dotNetRef;
+    var _moveHandler = null;
+    var _upHandler = null;
+
+    return {
+        register: function() {
+            _moveHandler = (e) => {
+                _dotNetRef.invokeMethodAsync('OnMouseMove', e.clientX);
+            };
+            _upHandler = (e) => {
+                _dotNetRef.invokeMethodAsync('OnMouseUp');
+                document.removeEventListener('mousemove', this._moveHandler);
+                document.removeEventListener('mouseup', this._upHandler);
+            };
+            document.addEventListener('mousemove', this._moveHandler);
+            documet.addEventListener('mouseup', this._upHandler);
+        },
+        unregister: function() {
+            documet.removeEventListener('mousemove', this._moveHandler);
+            documet.removeEventListener('mouseup', this._upHandler);
+            _dotNetRef = null;
+        }
+    }
+};
+
+export function register(dotNetRef) {
+    // TODO: return object to set to component JSObjectReference
+    mudDrawerResize.register(dotNetRef);
+}
+
+export function unregister() {
+    mudDrawerResize.unregister();
+}
